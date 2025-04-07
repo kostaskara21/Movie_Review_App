e69ee580945df87a5b95e6f9da30894d9d3b95b0
kostaskara21 <kostas.kara213@gmail.com>
2025-04-07 17:19:07 +0300

    Movie Index and Details Was Made

---
+++ CinemaApp/Controllers/MoviesController.cs
@@ -0,0 +1,42 @@
+﻿using CinemaApp.Data;
+using CinemaApp.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaApp.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MoviesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public IActionResult Index()
+        {
+            var Movies=_context.Movies.ToList();
+            return View(Movies);
+        }
+
+        public IActionResult Details(int id)
+        {
+            //Movie we want to Display Details
+            var DetailMovie=_context.Movies.FirstOrDefault(x => x.Id == id);
+
+            //Relared Movies
+            var MoreMovies=_context.Movies.Where(x=>x.Id != id).Take(5).ToList();
+            
+            //The ViewModel we made
+            var ViewModel = new MovieDetailViewModel
+            {
+                MyMovie = DetailMovie,
+                MoreMoviesM = MoreMovies
+            };
+
+            //Passing this to the View
+            return View(ViewModel);
+        }
+    }
+}
---
+++ CinemaApp/ViewModels/MovieDetailViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CinemaApp.Models;
+
+namespace CinemaApp.ViewModels
+{
+    //We are making this model in order to wrap the Movie we want to Display and also the List of related movies
+    public class MovieDetailViewModel
+    {
+        //The Movie we want to show Details For 
+        public Movies MyMovie { get; set; }
+
+        //The Related Movies We want to show 
+        public List<Movies> MoreMoviesM { get; set; }
+    }
+}
