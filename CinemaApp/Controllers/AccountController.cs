69950bdcf30d7d42fd3d9dd390a2cfd00b75d8ab
kostaskara21 <kostas.kara213@gmail.com>
2025-04-11 02:05:00 +0300

    Register and Logout Was Made

--- CinemaApp/Controllers/AccountController.cs
+++ CinemaApp/Controllers/AccountController.cs
@@ -48,6 +48,51 @@ namespace CinemaApp.Controllers
             return View(accountLogInDto);
             
         }
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(AccountSignUpDto accountSignUpDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(accountSignUpDto);
+            }
+            var user = await _userManager.FindByEmailAsync(accountSignUpDto.EmailAdress);
+            if (user != null)
+            {
+                TempData["SignUpErrror"] = "This Email Already Exists";
+               
+            }
+            var appuser = new AppUser
+            {
+                FirstName = accountSignUpDto.FirstName,
+                LastName = accountSignUpDto.LastName,
+                UserName=accountSignUpDto.EmailAdress,
+                Email= accountSignUpDto.EmailAdress
+            };
+            var newuser = await _userManager.CreateAsync(appuser, accountSignUpDto.Password);
+            if (newuser.Succeeded) {
+                await _userManager.AddToRoleAsync(appuser, "User");
+                TempData["SignUpNoError"] = "User Created Successfully.Please Sign In to use the Platform";
+                return View();
+            }
+            TempData["SignUpErrror"] = "Couldnt Create User Try Again(Pass must include at least one none alphanoumeric and number)";
+            //return RedirectToAction("Index", "Home");
+            return View();
+
+        }
+
+        
+        public async Task<IActionResult> LogOut()
+        {
+            await _signinManager.SignOutAsync();
+            return RedirectToAction("Index","Home");
+
+        }
 
     }
 }
---
+++ CinemaApp/Data/Dto/AccountSignUpDto.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace CinemaApp.Data.Dto
+{
+    public class AccountSignUpDto
+    {
+
+        [DisplayName("First Name")]
+        [Required]
+        public string FirstName { get; set; }
+
+
+        [DisplayName("Last Name")]
+        [Required]
+        public string LastName { get; set; }
+
+
+        [DisplayName("Email")]
+        [Required]
+        public string EmailAdress { get; set; }
+
+
+        [DisplayName("Password")]
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
